9ef593365134572da550a2364604e608b6907221
2014011377 <13717902399@163.com>
2016-10-12 22:26:42 +0800

    使用startService启动服务

---
+++ app/src/main/java/coml/example/administrator/exercise24/MainActivity.java
@@ -0,0 +1,36 @@
+package coml.example.administrator.exercise24;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonStart=(Button)findViewById(R.id.btnStartService);
+        Button buttonEnd=(Button)findViewById(R.id.btnStopService);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MyService.class);
+                intent.putExtra("num", 10);
+                startService(intent);
+            }
+        });
+
+        buttonEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MyService.class);
+                stopService(intent);
+            }
+        });
+
+    }
+}
